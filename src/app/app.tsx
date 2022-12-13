924716eb208d9d856b9859915e46dafba308e1e9
Hayden Crain <hayden@canva.com>
2022-12-13 20:31:53 +1100

    set up nodes and connections

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,17 +1,133 @@
+import React from 'react';
+import classNames from 'classnames';
 import './app.css';
-// import { HeroStarter } from '../hero_starter/hero_starter';
+import styles from './app.module.css';
+import { action, computed, makeObservable, observable } from 'mobx';
+import { observer } from 'mobx-react';
 
-function App() {
+export function App() {
   return (
-    <div className="app">
+    <div className={styles.app}>
       <div>Hello world</div>
-      <AndGate />
+      <Board />
     </div>
   );
 }
 
-export default App;
-
 function AndGate() {
-  return <div className="and">hello</div>;
+  return <div className={styles.and}>hello</div>;
+}
+
+const Board = observer(() => {
+  const [board] = React.useState(new BoardStore(['i1', 'i2'], ['o1']));
+  return (
+    <div>
+      <div>Inputs</div>
+      {board.inputs.map((input) => (
+        <div key={input.name}>
+          {`${input.name} `}
+          <span>
+            <InputNode input={input} />
+          </span>
+        </div>
+      ))}
+      <div>Outputs</div>
+      {board.outputs.map((output) => (
+        <div key={output.name}>
+          {`${output.name} `}
+          <span>
+            <OutputNode output={output} />
+          </span>
+        </div>
+      ))}
+      <button onClick={() => board.connect('i1', 'o1')}>connect</button>
+      <button onClick={() => board.disconnect('o1')}>disconnect</button>
+    </div>
+  );
+});
+
+const InputNode = observer(({ input }: { input: Input }) => {
+  return (
+    <div onClick={input.flip} className={classNames(styles.node, { [styles.on]: input.value })} />
+  );
+});
+
+const OutputNode = observer(({ output }: { output: Output }) => {
+  return <div className={classNames(styles.node, { [styles.on]: output.value })} />;
+});
+
+class BoardStore {
+  readonly inputs: Input[];
+  readonly outputs: Output[];
+
+  constructor(inputNames: string[], outputNames: string[]) {
+    this.inputs = inputNames.map((name) => new Input(name));
+    this.outputs = outputNames.map((name) => new Output(name));
+
+    makeObservable(this, {
+      inputs: observable.deep,
+      outputs: observable.deep,
+      connect: action.bound
+    });
+  }
+
+  connect(inputName: string, outputName: string) {
+    const input = this.inputs.find((input) => input.name === inputName);
+    const output = this.outputs.find((output) => output.name === outputName);
+    if (!input || !output) {
+      return;
+    }
+    output.connect(input);
+  }
+
+  disconnect(outputName: string) {
+    const output = this.outputs.find((output) => output.name === outputName);
+    if (!output) {
+      return;
+    }
+    output.disconnect();
+  }
+}
+
+class Input {
+  value: boolean = false;
+
+  constructor(readonly name: string) {
+    makeObservable(this, {
+      value: observable.ref,
+      flip: action.bound
+    });
+  }
+
+  readonly flip = () => {
+    this.value = !this.value;
+  };
+}
+
+class Output {
+  input: Input | undefined = undefined;
+
+  constructor(readonly name: string) {
+    makeObservable(this, {
+      value: computed,
+      input: observable.ref,
+      connect: action.bound,
+      disconnect: action.bound
+    });
+  }
+
+  connect(input: Input) {
+    this.input = input;
+  }
+
+  disconnect() {
+    this.input = undefined;
+  }
+
+  get value() {
+    if (!this.input) {
+      return false;
+    }
+    return this.input.value;
+  }
 }
--- src/gates/gates.ts
+++ src/gates/gates.ts
@@ -1,32 +1,43 @@
-import { action, computed, IObservableValue, observable } from 'mobx';
+import { action, computed, IObservableValue, makeObservable, observable } from 'mobx';
 
 export class Input {
   value: IObservableValue<boolean> = observable.box(false);
 
-  @action
+  constructor() {
+    makeObservable(this, {
+      set: action,
+      get: computed
+    });
+  }
+
   set(value: boolean) {
     this.value.set(value);
   }
 
-  @computed
   get() {
     return this.value.get();
   }
 }
 
 export class And {
-  constructor(private readonly w1: Input, private readonly w2: Input) {}
+  constructor(private readonly w1: Input, private readonly w2: Input) {
+    makeObservable(this, {
+      output: computed
+    });
+  }
 
-  @computed
   get output() {
     return this.w1.get() && this.w2.get();
   }
 }
 
 export class Not {
-  constructor(private readonly w1: Input) {}
+  constructor(private readonly w1: Input) {
+    makeObservable(this, {
+      output: computed
+    });
+  }
 
-  @computed
   get output() {
     return !this.w1.get();
   }
